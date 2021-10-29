f4f2c1044a3c1e1a26ff0727808f0c1547e05d57
Diego López <dieguitarra@gmail.com>
2021-10-29 14:51:55 -0300

    Se agregó todo

---
+++ desafio1.js
@@ -0,0 +1,7 @@
+let nombreUsuario = prompt("Ingresa tu nombre");
+let apellidoUsuario = prompt("Ingresa tu apellido");
+console.log("Bienvenido al sistema " + nombreUsuario);
+let numeroUno = parseInt(prompt("Ingrese un número"));
+let numeroDos = parseInt(prompt("Ingrese otro número"));
+let suma = parseInt(numeroUno) + parseInt(numeroDos);
+console.log(numeroUno + numeroDos);
